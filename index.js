28a383614d7e54da08d3b009991c2d819024a26f
Stephon Ricks <53449254+xFallingDuskx@users.noreply.github.com>
2022-10-24 23:55:34 +0200

    changed submit btn design and location

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function getGameInfo() {
     const numOfPlayersInput = document.querySelector(
         'input[name="btn-selection-players"]:checked'
     );
-    const numOfPlayers = numOfPlayersInput == null ? alert("missing # of players") : numOfPlayersInput.value;
+    const numOfPlayers = numOfPlayersInput == null
+        ? alert("missing # of players")
+        : numOfPlayersInput.value;
 
     console.log("Selected num of players is " + numOfPlayers);
 }
