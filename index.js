68db679b53700681d9fa49d65d505d819b707239
Stephon Ricks <53449254+xFallingDuskx@users.noreply.github.com>
2022-10-25 14:55:48 +0200

    removed log statements

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function changeInputBackground(field) {
-    console.log("The value is " + field.value);
     if (field.value != "") {
         field.style.backgroundColor = "rgb(245, 245, 245, 0.7)";
     } else {
@@ -15,8 +14,6 @@ function getGameInfo() {
         ? alert("missing # of players")
         : numOfPlayersInput.value;
 
-    console.log("Selected num of players is " + numOfPlayers);
-
     const teamOneMembersInput = document
         .getElementById("teams-of-one")
         .value
@@ -31,13 +28,9 @@ function getGameInfo() {
     if ((teamOneMembersInput.length != numOfPlayers / 2) || (teamTwoMembersInput.length != numOfPlayers / 2)) {
         alert("# of team members do not match selected teams' size")
     }
-
-    console.log("For team one: " + teamOneMembersInput);
-    console.log("For team two: " + teamTwoMembersInput);
 }
 
 function setInputBackground(field) {
-    console.log("The value is " + field.value);
     if (field.value != "") {
         field.style.backgroundColor = "white";
     } else {
