4cb97342d69a2148c52fc95f45b58d2fe0178a21
Stephon Ricks <53449254+xFallingDuskx@users.noreply.github.com>
2022-10-27 23:28:04 +0200

    added welcome text in typing animation

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ let winningScore = 400;
 let gameScreenEl = document.getElementById("game-screen");
 let numBidsIndicator = document.getElementById("num-bids-indicator");
 let mainPanelEl = document.getElementById("main-panel");
+let welcomeTextEl = document.getElementById("welcome-text");
 let winningScoreIndicator = document.getElementById("winning-score-indicator");
+
+const welcomeTextOne = "Seems like you're all set to get started! Best of luck to both teams!";
+const welcomeTextTwo = "Oh and do try not to renege. No one likes that person " +
+        String.fromCodePoint(0x1F608) + " except the other team, I mean.";
 
 function changeInputBackground(field) {
     if (field.value != "") {
@@ -16,16 +21,18 @@ function changeInputBackground(field) {
 
 function closePanel() {
     if (window.innerWidth <= 576) {
-        mainPanelEl.classList.add("slide-up");
+        mainPanelEl
+            .classList
+            .add("slide-up");
         mainPanelEl.addEventListener("animationend", switchToGameScreen(), false);
     }
     return;
 }
 
 function getGameInfo() {
+    // TODO - Remove, for testing
     closePanel();
     return;
-
 
     const numOfPlayersInput = document.querySelector(
         'input[name="btn-selection-players"]:checked'
@@ -52,6 +59,9 @@ function getGameInfo() {
         alert("# of team members do not match selected teams' size");
         return;
     }
+
+    closePanel();
+    return;
 }
 
 function setInputBackground(field) {
@@ -76,7 +86,31 @@ function switchToGameScreen() {
             mainPanelEl.style.display = "none";
             gameScreenEl.style.display = "flex";
             gameScreenEl.style.flex = "1";
+            welcomeUsers(1);
         }, 1500);
     }
+
     return;
+}
+
+let idx1 = 0
+let idx2 = 0;
+
+function welcomeUsers(num) {
+    if (num == 1) {
+        welcomeTextEl.textContent += welcomeTextOne.charAt(idx1);
+        idx1++;
+        if (idx1 == welcomeTextOne.length) {
+            num = 2;
+            welcomeTextEl.textContent += "\r\n\r\n\r\n";
+        }
+        setTimeout(welcomeUsers, 50, num);
+    } else {
+        welcomeTextEl.textContent += welcomeTextTwo.charAt(idx2);
+        idx2++;
+        if (idx2 == welcomeTextTwo.length) {
+            return
+        }
+        setTimeout(welcomeUsers, 50, num);
+    }
 }
